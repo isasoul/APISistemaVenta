793be927d57b1bc4fa304be719e55823a61a2977
Isabel Molina <isabel.molina@joinsolutions.net>
2023-12-09 16:37:10 -0300

    subo cambios Entity

---
+++ SistemaVenta.Model/YourOutputDirectory/MasterContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaVenta.Model.YourOutputDirectory;
+
+public partial class MasterContext : DbContext
+{
+    public MasterContext()
+    {
+    }
+
+    public MasterContext(DbContextOptions<MasterContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS01;Database=master;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
